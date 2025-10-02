22b5be71ae739ace04317a88ba0db93fbc2a3e9f
stan-linskens <89661@roc-teraa.nl>
2025-10-02 10:22:55 +0200

    css - register

--- LaraCrud/resources/views/Login-Register/register.blade.php
+++ LaraCrud/resources/views/Login-Register/register.blade.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="css/style.css" rel="stylesheet">
+    </link>
+    <title>Netflix Clone - Register</title>
 </head>
-<body>
 
-    @Auth
-    <p>logged in</p>
-    <form action="/logout" method="POST">
-       @csrf 
-       <button>log out</button>
-    </form>
-    @else
-    <h2>Register</h2>
-    <form action="/register" method="POST">
-        @csrf
-        <input name="name" type="text" placeholder="name">
-        <input name="email" type="email" placeholder="email">
-        <input name="password" type="password" placeholder="password">
-        <button>Register</button>
-    </form>
-    <h2>login</h2>
-    <form action="/login" method="POST">
-        @csrf
-        <input name="loginname" type="text" placeholder="name">
-        <input name="loginpassword" type="password" placeholder="password">
-        <button>log in</button>
-    </form>
-    @endauth
+<body id="auth-body">
+    <div class="auth-container">
+        @Auth
+        <p>logged in</p>
+        <form action="/logout" method="POST">
+            @csrf
+            <button class="secondary-btn">log out</button>
+        </form>
+        @else
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="email" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+        <h2>login</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input name="loginname" type="text" placeholder="name">
+            <input name="loginpassword" type="password" placeholder="password">
+            <button>log in</button>
+        </form>
+        @endauth
+    </div>
 </body>
+
 </html>
